663f272e30c926dfe0cabcf69796860cf510ca55
Priyanka Rajput <025.priyanka@gmail.com>
2020-05-18 00:21:34 +0530

    here_maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HereMapComponent } from './here-map/here-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HereMapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/here-map/here-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-here-map',
+  templateUrl: './here-map.component.html',
+  styleUrls: ['./here-map.component.css']
+})
+export class HereMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

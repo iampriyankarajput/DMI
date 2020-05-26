86e99f80746838d35df06c72bf4178a71117806e
iampriyankarajput <52007136+iampriyankarajput@users.noreply.github.com>
2020-05-26 19:25:46 +0530

    Designs register page & integrates register API (#3)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { HereMapComponent } from './components/Search/search.component';
+import { SearchComponent } from './components/Search/search.component';
 import { AuthGuard } from "./services/auth.guard";
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   { path: "", component: LoginComponent },
-  { path: "search",component: HereMapComponent, canActivate: [AuthGuard]}
+  { path: "signup", component: SignupComponent },
+  { path: "search",component: SearchComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
--- src/app/components/Search/search.component.ts
+++ src/app/components/Search/search.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
+
 declare var H: any;
 
 @Component({
@@ -8,39 +9,40 @@ declare var H: any;
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class HereMapComponent implements OnInit {
+export class SearchComponent implements OnInit {
+
   private platform: any;
 
-  @ViewChild("map")
-  public mapElement: ElementRef;
-
   constructor(private api: ApiService, private router: Router) {
+    // initialize communication with the platform
     this.platform = new H.service.Platform({
       apikey: 'HP392wrN-Ab_L4SUrK3lbj_uBk4TZaASf-kUizd8rNM'
     });
   }
 
   ngOnInit(): void {
-
+    // initialize a map
     let defaultLayers = this.platform.createDefaultLayers();
     let map = new H.Map(document.getElementById('map'),
       defaultLayers.vector.normal.map, {
-      center: { lat: 20.5, lng: 78.9 }, 
+      center: { lat: 20.5, lng: 78.9 },
       zoom: 5,
       pixelRatio: window.devicePixelRatio || 1
     });
 
+    // add a resize listener
     window.addEventListener('resize', () => map.getViewPort().resize());
 
-
+    // make the map interactive
     var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
 
+    //  Create the default UI components
     var ui = H.ui.UI.createDefault(map, defaultLayers);
   }
 
   // here logout function works
-  logout() {
-    this.api.doLogout()
+  logOut() {
+    this.api.doLogOut()
     this.router.navigate(['/']);
   }
 
